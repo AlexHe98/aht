5cc58d1669c8451abc4d58f3345b4511b3dcd91c
alex-he <mr.a.he98@gmail.com>
2024-02-10 19:26:14 +1000

    Start a test suite for the Pairing class, and start writing test cases for Pairing.contract().

---
+++ testpairing.py
@@ -0,0 +1,23 @@
+"""
+Test suite for the Pairing class.
+"""
+from pairing import Pairing
+from orbiterror import PairingNotDisjoint
+
+
+def testPairing():
+    print()
+
+    # Test Pairing.contract().
+    print( "============================================================" )
+    print( " Pairing.contract() " )
+    print( "------------------------------------------------------------" )
+    print()
+    #TODO
+    # End of test suite.
+    print( "PASSED!" )
+    return
+
+
+if __name__ == "__main__":
+    testPairing()
