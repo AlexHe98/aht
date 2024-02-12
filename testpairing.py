a8439c977d28bb201394de932f91e714fead79d0
alex-he <mr.a.he98@gmail.com>
2024-02-13 09:53:13 +1000

    Fill in test cases for legality-checking in Pairing.contract( start, width ).

--- testpairing.py
+++ testpairing.py
@@ -12,11 +12,30 @@ def testPairing():
     print( " Pairing.contract() " )
     print( "------------------------------------------------------------" )
     print()
-    #TODO
-    if Pairing(1,2,3,True).contract(2,2):
-        # This contraction should be illegal.
-        raise RuntimeError( "FAILED." )
-    print()
+    contractTestCases = [
+            [ 3, 7, 2, True, 1, 2, True ],
+            [ 3, 7, 2, False, 5, 2, True ],
+            [ 3, 7, 2, True, 9, 2, True ],
+            [ 3, 7, 2, False, 2, 2, False ],
+            [ 3, 7, 2, True, 4, 2, False ],
+            [ 3, 7, 2, False, 2, 4, False ],
+            [ 3, 7, 2, True, 6, 3, False ],
+            [ 3, 7, 2, False, 7, 3, False ],
+            [ 3, 7, 2, True, 7, 2, False ],
+            [ 3, 7, 2, False, 3, 7, False ],
+            [ 1, 2, 3, True, 2, 2, False ] ]
+    for a, c, pWidth, preserving, cStart, cWidth, legal in contractTestCases:
+        pairing = Pairing( a, c, pWidth, preserving )
+        msg = "{}. Contracting [{},{}] is ".format(
+                pairing, cStart, cStart + cWidth - 1 )
+        if legal:
+            msg += "legal."
+        else:
+            msg += "illegal."
+        print(msg)
+        if pairing.contract( cStart, cWidth ) != legal:
+            raise RuntimeError( "FAILED." )
+        print()
     # End of test suite.
     print( "PASSED!" )
     return
