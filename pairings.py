44e89b6bd08061339798862b8d734d37ee4b7561
alex-he <mr.a.he98@gmail.com>
2024-03-19 20:03:31 +1000

    Fill in some basic functionality for the Pairings class.

--- pairings.py
+++ pairings.py
@@ -10,22 +10,39 @@ class Pairings:
     A pseudogroup of pairings between subintervals of { 1, ..., N }, for some
     positive integer N.
     """
-    def __init__(self):
+    def __init__( self, pairings=None ):
         """
+        Initialises a pseudogroup of interval pairings consisting of the
+        given collection of pairings.
+
+        If no pairings are given, then the initialised pseudogroup will be
+        empty.
         """
-        #TODO
-        pass
+        self._pairings = set(pairings)
 
     def __str__(self):
-        #TODO
-        pass
+        return "Order-{} pseudogroup of interval pairings".format(
+                self.order() )
 
     def __repr__(self):
-        #TODO
-        pass
+        return "Pairings( {} )".format( repr(self._pairings) )
 
     def detail(self):
         """
+        Returns a detailed text representation of this pseudogroup of
+        interval pairings.
+
+        This text may span many lines, and provides a human-readable
+        description that completely specifies this pseudogroup. This text
+        always ends with a final newline.
         """
-        #TODO
-        pass
+        msg = ""
+        for p in self._pairings:
+            msg += "{}\n".format(p)
+        return msg
+
+    def order(self):
+        """
+        Returns the number of interval pairings in this pseudogroup.
+        """
+        return len(self._pairings)
